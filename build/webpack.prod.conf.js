45fa83c6d3ff0722c2280ff7e3f4ac99e30fb8f1
HaoChuan9421 <hc199421@gmail.com>
2018-12-27 22:50:34 +0800

    添加CNAME

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -116,6 +116,10 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../CNAME'),
+        to: config.dev.assetsRoot
       }
     ])
   ],
