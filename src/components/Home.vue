3935cb4b45e32887acf97dc109206b0937eb7865
HaoChuan9421 <hc199421@gmail.com>
2021-04-07 01:23:15 +0800

    feat: 更新依赖

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="hello">
     <div @click="showData" class="preview" v-html="msg"></div>
-    <!-- 你不需要担心一个页面中过多vue-ueditor-wrap组件会导致混乱,或者重复引用JS(打开控制台瞄一眼),每个组件都保证拥有一个独立的UEditor实例,你可以尝试把数字改成99(根据自己电脑自行斟酌),但依然稳定可用 -->
-    <vue-ueditor-wrap ref="ueditor" v-model="msg" :destroy="false" :config="config" @ready="ready" v-for="item in 2" :key="item" @beforeInit="addCustomUI"></vue-ueditor-wrap>
+    <vue-ueditor-wrap v-model="msg" :config="editorConfig" @ready="ready" v-for="item in 2" :key="item" @before-init="addCustomUI"></vue-ueditor-wrap>
   </div>
 </template>
 
 <script>
 // 1、引入VueUeditorWrap组件
-import VueUeditorWrap from 'vue-ueditor-wrap' // ES6 Module
-// const VueUeditorWrap = require('vue-ueditor-wrap') // CommonJS
+import VueUeditorWrap from 'vue-ueditor-wrap'
 export default {
   name: 'HelloWorld',
   // 2、注册组件
@@ -20,22 +18,16 @@ export default {
     return {
       // 3、v-model绑定数据
       msg: '<h2><img src="http://img.baidu.com/hi/jx2/j_0003.gif"/>Vue + UEditor + v-model双向绑定</h2>',
-      // 4、根据项目需求自行配置,具体配置参见ueditor.config.js源码或 http://fex.baidu.com/ueditor/#start-start
-      config: {
-        // 编辑器不自动被内容撑高
-        autoHeightEnabled: false,
+      // 4、根据项目需求自行配置,具体配置参见ueditor.config.js源码或 http://fex.baidu.com/ueditor/#start-config
+      editorConfig: {
         // 初始容器高度
         initialFrameHeight: 240,
         // 初始容器宽度
         initialFrameWidth: '100%',
-        // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
-        serverUrl: 'http://35.201.165.105:8000/controller.php'
-        // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
-        // UEDITOR_HOME_URL: '/static/UEditor/'
-        // 配合最新编译的资源文件，你可以实现添加自定义Request Headers,详情https://github.com/HaoChuan9421/ueditor/commits/dev-1.4.3.3
-        // headers: {
-        //   access_token: '37e7c9e3fda54cca94b8c88a4b5ddbf3'
-        // }
+        // 访问 UEditor 静态资源的根路径，可参考 https://hc199421.gitee.io/vue-ueditor-wrap/#/faq
+        UEDITOR_HOME_URL: '/static/UEditor/',
+        // 服务端接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
+        serverUrl: '//ueditor.szcloudplus.com/cos'
       }
     }
   },
@@ -52,7 +44,7 @@ export default {
       alert(this.msg)
       console.log(this.msg)
     },
-    // 7. 借助 beforeInit 钩子，你可以实现对 UEditor 的二次开发，会在 scripts 加载完毕之后、编辑器初始化之前触发，以 编辑器 id 和 配置参数 作为入参
+    // 7. 借助 before-init 钩子，你可以实现对 UEditor 的二次开发，会在 scripts 加载完毕之后、编辑器初始化之前触发，以 编辑器 id 和 配置参数 作为入参
     addCustomUI (editorId, editorConfig) {
       console.log(editorId + '的配置参数是:', JSON.stringify(editorConfig, null, 2))
 
@@ -61,7 +53,7 @@ export default {
         // 注册按钮执行时的 command 命令，使用命令默认就会带有回退操作
         editor.registerCommand(uiName, {
           execCommand: function () {
-            editor.execCommand('inserthtml', `<span style="color: #${editorId.substr(-3)};">这是一段由自定义按钮添加的文字，你点击的编辑器ID是${editorId}</span>`)
+            editor.execCommand('inserthtml', `<span style="color: #${Math.random().toString(16).substr(-3)};">这是一段由自定义按钮添加的文字，你点击的编辑器ID是${editorId}</span>`)
           }
         })
 
